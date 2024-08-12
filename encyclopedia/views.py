05bacb164df0fcab14424c195f8398a345662259
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2024-08-12 01:06:11 +0100

    Adds error if you try to make an entry that exists

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django import forms
 from . import util
 import re
+from django.contrib import messages
 
 import markdown2
 
@@ -51,8 +52,13 @@ def new_entry(request):
     form_title = request.POST.get("entry_title")
     form_content = request.POST.get("entry_content")
 
-    if util.get_entry(form_title):  # Might need to change to is not None
-        return HttpResponseBadRequest("We already have a wiki page for this.")
+    if util.get_entry(form_title):
+        # return HttpResponseBadRequest("We already have a wiki page for this.")
+        messages.error(
+            request,
+            f"We already have a page for '{form_title}', why don't you make a page about something else.",
+        )
+        return redirect("new_entry")
 
     util.save_entry(form_title, form_content)
     return redirect(f"/wiki/{form_title}")
